2f7b1318fa9ecbf1526c8b3870edce4fed083830
Dmitriy Shaplov <shaplovd@gmail.com>
2018-12-01 12:31:12 +0300

    1.3 JavaDoc. Code convention [#91672]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Calculate решение задачи части 001 урок 1.3. 
+ * @author shaplov
+ * @since 01.12.2018
+ */
 public class Calculate {
+	
+	/**
+	* Main. Вывод строки в консоль.
+	* @param args - args.
+	*/	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Dmitriy Shaplov (dshaplov@gmail.com)
+* @version 1
+* @since 01.12.2018
+*/
+package ru.job4j.calculate;
