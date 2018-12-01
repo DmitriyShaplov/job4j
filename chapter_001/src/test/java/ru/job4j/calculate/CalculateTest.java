7fff2885a8bd985faebeddadecc74f60059603eb
Dmitriy Shaplov <shaplovd@gmail.com>
2018-12-01 23:25:58 +0300

    1.4. JUnit[#91673]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ * @author shaplov
+ * @version $Id$
+ * @since 01.12.2018
+ */
+public class CalculateTest {
+	/**
+	* Test echo.
+	*/
+	@Test
+	public void whenTakeNameThenThreeEchoPlusName() {
+		String input = "Dmitriy Shaplov";	
+		String expect = "Echo, echo, echo : Dmitriy Shaplov";
+		//Создание нового объекта.
+		Calculate calc = new Calculate();
+		//Выполнение метода echo с параметром input и запись ее в переменную result.
+		String result = calc.echo(input); 
+		assertThat(result, is(expect));
+	}
+}
