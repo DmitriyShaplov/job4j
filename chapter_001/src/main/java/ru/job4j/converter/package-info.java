e344d072c6fa53f7dd51f12ce9b365b9c51cf7d9
Dmitriy Shaplov <shaplovd@gmail.com>
2018-12-05 23:06:02 +0300

    3.2. Конвертер валюты.[#91660]

---
+++ chapter_001/src/main/java/ru/job4j/converter/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package for converter task.
+ *
+ * @author Dmitriy Shaplov (dshaplov@gmail.com)
+ * @version 1
+ * @since 05.12.2018
+ */
+
+package ru.job4j.converter;
---
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test for converter.
+ * @author shaplov
+ * @version $Id$
+ * @since 04.12.2018
+ */
+public class ConverterTest {
+
+    /**
+     * Test rubleToDollar
+     */
+    @Test
+    public void when60RubleToDollarThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(60);
+        assertThat(result, is(1));
+    }
+
+    /**
+     * Test rubleToEuro
+     */
+    @Test
+    public void when70RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToEuro(70);
+        assertThat(result, is(1));
+    }
+
+    /**
+     * Test dollarToRuble
+     */
+    @Test
+    public void when1DollarToRubleThen60() {
+        Converter converter = new Converter();
+        int result = converter.dollarToRuble(1);
+        assertThat(result, is(60));
+    }
+
+    /**
+     * Test EuroToRuble
+     */
+    @Test
+    public void when1EuroToRubleThen70() {
+        Converter converter = new Converter();
+        int result = converter.EuroToRuble(1);
+        assertThat(result, is(70));
+    }
+}
