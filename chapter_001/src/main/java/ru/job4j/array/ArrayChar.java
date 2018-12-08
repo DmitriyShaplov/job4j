6b95d50191336b08792728e9f7043f199220ccfd
Dmitriy Shaplov <shaplovd@gmail.com>
2018-12-08 14:21:24 +0300

    6.4. Слово начинается с ...[#91652]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+/**
+ * Обертка над строкой.
+ * @author shaplov
+ * @version 1.00
+ * @since 08.12.2018
+ */
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    /**
+     * Проверяет, начинается ли слово с префикса
+     * @param prefix префикс
+     * @return result true/false
+     */
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        if (value.length > data.length) {
+            result = false;
+        } else {
+            for (int i = 0; i < value.length; i++) {
+                if (value[i] != data[i]) {
+                    result = false;
+                }
+            }
+        }
+        return result;
+    }
+}
