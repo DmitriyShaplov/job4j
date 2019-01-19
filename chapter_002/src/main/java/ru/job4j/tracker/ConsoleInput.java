66fedcad3b79e47847724ccff36888e5b820ed99
Dmitriy Shaplov <shaplovd@gmail.com>
2019-01-19 14:34:57 +0300

    3. if-else-throw[#91716]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -1,30 +1,55 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * @author shaplov
+ * @version $Id$
+ */
 public class ConsoleInput implements Input {
 
+    private List<Validate> validates = new ArrayList<>();
     private Scanner scanner = new Scanner(System.in);
+
+    private void fillValidates() {
+        this.validates.add(new Validate() {
+            public void validate(int key, List<Integer> range) {
+                if (ConsoleInput.this.isInRange(key, range)) {
+                    throw new MenuOutException("Out of menu range.");
+                }
+            }
+        });
+    }
 
     public String ask(String question) {
         System.out.print(question);
         return scanner.nextLine();
     }
 
+    /**
+     * Check if there is a key in range
+     * @param key value
+     * @param range list
+     * @return result
+     */
+    private boolean isInRange(int key, List<Integer> range) {
+        boolean exists = false;
+        for (int num : range) {
+            if (key == num) {
+                exists = true;
+                break;
+            }
+        }
+        return exists;
+    }
+
     public int ask(String question, List<Integer> range) {
          int key = Integer.valueOf(ask(question));
-         boolean exists = false;
-         for (int num : range) {
-             if (key == num) {
-                 exists = true;
-                 break;
-             }
+         for (Validate val : validates) {
+             val.validate(key, range);
          }
-         if (exists) {
-             return key;
-         } else {
-             throw new MenuOutException("Out of menu range.");
-         }
+         return key;
     }
 }
---
+++ chapter_002/src/main/java/ru/job4j/tracker/Validate.java
@@ -0,0 +1,7 @@
+package ru.job4j.tracker;
+
+import java.util.List;
+
+public interface Validate {
+    void validate(int key, List<Integer> range);
+}
