4c6aa8c8f8757a8116fccb18a071cde820712212
Dmitriy Shaplov <shaplovd@gmail.com>
2019-02-18 14:54:56 +0300

    2. Очередь с приоритетом на LinkedList[#91696]

---
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,26 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     * Inserts task to desired position.
+     * Position determinate by the priority field.
+     * @param task task
+     */
+    public void put(Task task) {
+        for (int index = 0; index <= tasks.size(); index++) {
+            if (index == tasks.size()
+                    || tasks.get(index).getPriority() > task.getPriority()) {
+                tasks.add(index, task);
+                break;
+            }
+        }
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/search/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for search.
+ *
+ * @author Dmitriy Shaplov (dshaplov@gmail.com)
+ * @since 0.1
+ */
+
+package ru.job4j.search;
