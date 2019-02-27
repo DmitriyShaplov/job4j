16b9f870c3353c92ca9e9b936216b20b7b542ce7
Dmitriy Shaplov <shaplovd@gmail.com>
2019-02-27 16:00:38 +0300

    1. Все задачи из 3 части переделать на Stream API.[#91601] update1

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -1,6 +1,8 @@
 package ru.job4j.search;
 
 import java.util.LinkedList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
@@ -11,13 +13,19 @@ public class PriorityQueue {
      * @param task task
      */
     public void put(Task task) {
-        for (int index = 0; index <= tasks.size(); index++) {
-            if (index == tasks.size()
-                    || tasks.get(index).getPriority() > task.getPriority()) {
-                tasks.add(index, task);
-                break;
-            }
-        }
+        this.tasks = Stream.concat(Stream.concat(tasks.stream().filter(
+                t -> t.getPriority() < task.getPriority()
+        ), Stream.of(task)),
+                tasks.stream().filter(
+                t -> t.getPriority() >= task.getPriority()
+        )).collect(Collectors.toCollection(LinkedList::new));
+//        for (int index = 0; index <= tasks.size(); index++) {
+//            if (index == tasks.size()
+//                    || tasks.get(index).getPriority() > task.getPriority()) {
+//                tasks.add(index, task);
+//                break;
+//            }
+//        }
     }
 
     /**
--- chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -15,4 +15,14 @@ public class PriorityQueueTest {
         Task result = queue.take();
         assertThat(result.getDesc(), is("urgent"));
     }
+
+    @Test
+    public void whenHighterPriorityLast() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 5));
+        queue.put(new Task("middle", 3));
+        queue.put(new Task("urgent", 1));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
 }
