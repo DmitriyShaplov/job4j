94809448d75f4eb32c7f12bf0987986cc8ca6426
Dmitriy Shaplov <shaplovd@gmail.com>
2019-03-15 18:18:49 +0300

    Статистику по коллекции.[#117764] проверка производительности по сравнению с перебором

--- collections_pro/src/test/java/ru/job4j/tasks/AnaliseTest.java
+++ collections_pro/src/test/java/ru/job4j/tasks/AnaliseTest.java
@@ -2,6 +2,7 @@ package ru.job4j.tasks;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -17,25 +18,25 @@ public class AnaliseTest {
     @Test
     public void whenFiveUsersThenAddTreeDeleteTwoAndChangeOneResult321() {
         Analise analise = new Analise();
-        var info = analise.diff(
-                List.of(
-                        new Analise.User(1, "test1"),
-                        new Analise.User(2, "test2"),
-                        new Analise.User(3, "test3"),
-                        new Analise.User(4, "test4"),
-                        new Analise.User(5, "test5")
-                ),
-                List.of(
-                        new Analise.User(1, "test1"),
-                        new Analise.User(4, "test44"),
-                        new Analise.User(5, "test5"),
-                        new Analise.User(6, "test2"),
-                        new Analise.User(7, "test3"),
-                        new Analise.User(8, "test4")
-                )
-        );
+        List<Analise.User> listPrv = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            listPrv.add(new Analise.User(i, String.format("test%d", i)));
+        }
+        var listCur = new ArrayList<>(listPrv);
+        for (int i = 7000; i < 10000; i++) {
+            listCur.set(i, new Analise.User(i + 3000, String.format("test%d", i + 3000)));
+        }
+        for (int i = 5000; i < 7000; i++) {
+            listCur.set(i, new Analise.User(i, String.format("test%d", i + 3000)));
+        }
+        for (int i = 0; i < 500; i++) {
+            listCur.remove(0);
+        }
+        var time = System.currentTimeMillis();
+        var info = analise.diff(listPrv, listCur);
+        System.out.println(System.currentTimeMillis() - time);
         int[] result = {info.getAdded(), info.getDeleted(), info.getChanged()};
-        int[] expect = {3, 2, 1};
+        int[] expect = {3000, 3500, 2000};
         assertThat(result, is(expect));
     }
 }
