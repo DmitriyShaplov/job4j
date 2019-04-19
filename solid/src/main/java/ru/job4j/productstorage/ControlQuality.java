e92f8603604347683f116ab3f589b1119ef2e426
Dmitriy Shaplov <shaplovd@gmail.com>
2019-04-19 14:49:48 +0300

    1. Динамическое перераспределение продуктов[#117894]

--- solid/src/main/java/ru/job4j/productstorage/ControlQuality.java
+++ solid/src/main/java/ru/job4j/productstorage/ControlQuality.java
@@ -3,7 +3,9 @@ package ru.job4j.productstorage;
 import ru.job4j.productstorage.products.Food;
 import ru.job4j.productstorage.storages.Storage;
 
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ControlQuality {
@@ -26,6 +28,19 @@ public class ControlQuality {
             if (storage.addProduct(product)) {
                 break;
             }
+        }
+    }
+
+    /**
+     * Resorts all products.
+     */
+    public void resort() {
+        List<Food> products = new ArrayList<>();
+        for (Storage storage : storages) {
+            products.addAll(storage.pickUpAll());
+        }
+        for (Food product : products) {
+            distribute(product);
         }
     }
 }
--- solid/src/test/java/ru/job4j/productstorage/ControlQualityTest.java
+++ solid/src/test/java/ru/job4j/productstorage/ControlQualityTest.java
@@ -2,13 +2,9 @@ package ru.job4j.productstorage;
 
 import org.junit.Test;
 import ru.job4j.productstorage.products.*;
-import ru.job4j.productstorage.storages.Shop;
-import ru.job4j.productstorage.storages.Storage;
-import ru.job4j.productstorage.storages.Trash;
-import ru.job4j.productstorage.storages.Warehouse;
+import ru.job4j.productstorage.storages.*;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.core.Is.is;
@@ -73,6 +69,60 @@ public class ControlQualityTest {
         String shopList = String.join(":", shop.getProducts().stream().map(Food::getName).collect(Collectors.toList()));
         String warehouseList = String.join(":", warehouse.getProducts().stream().map(Food::getName).collect(Collectors.toList()));
         String trashList = String.join(":", trash.getProducts().stream().map(Food::getName).collect(Collectors.toList()));
+        String expectShop = ("Herring:32 cows");
+        String expectWH = ("Chicken");
+        String expectTrash = ("Mackerel:Apple");
+        assertThat(shopList, is(expectShop));
+        assertThat(warehouseList, is(expectWH));
+        assertThat(trashList, is(expectTrash));
+    }
+
+    @Test
+    public void whenChangeShopThenResortProductsThenTheyInNewShop() {
+        Storage trash = new TrashMock(new Trash());
+        Storage warehouse = new Warehouse();
+        Storage shop = new Shop(25);
+        ControlQuality controlQuality = new ControlQuality();
+        controlQuality.addStorage(shop);
+        controlQuality.addStorage(warehouse);
+        controlQuality.addStorage(trash);
+        controlQuality.distribute(
+                new Fish("Mackerel",
+                        LocalDate.now().minusDays(5),
+                        LocalDate.now().minusDays(1),
+                        32500)
+        );
+        controlQuality.distribute(
+                new Fish("Herring",
+                        LocalDate.now().minusDays(10),
+                        LocalDate.now().plusDays(1),
+                        22200)
+        );
+        controlQuality.distribute(
+                new Meat("Chicken",
+                        LocalDate.now().minusDays(1),
+                        LocalDate.now().plusDays(8),
+                        17000)
+        );
+        controlQuality.distribute(
+                new MilkProducts("32 cows",
+                        LocalDate.now().minusDays(5),
+                        LocalDate.now().plusDays(10),
+                        6055)
+        );
+        controlQuality.distribute(
+                new Fruits("Apple",
+                        LocalDate.now().minusDays(5),
+                        LocalDate.now().minusDays(2),
+                        9500)
+        );
+        ((TrashMock) trash).setCanStore(false);
+        Storage trashAfterResort = new Trash();
+        controlQuality.addStorage(trashAfterResort);
+        controlQuality.resort();
+        String shopList = String.join(":", shop.getProducts().stream().map(Food::getName).collect(Collectors.toList()));
+        String warehouseList = String.join(":", warehouse.getProducts().stream().map(Food::getName).collect(Collectors.toList()));
+        String trashList = String.join(":", trashAfterResort.getProducts().stream().map(Food::getName).collect(Collectors.toList()));
         String expectShop = ("Herring:32 cows");
         String expectWH = ("Chicken");
         String expectTrash = ("Mackerel:Apple");
